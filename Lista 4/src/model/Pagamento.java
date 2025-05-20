4edd5be9a0818323ae3c5d253314d8c567b20555
Gustavo.B <britogustavo88@gmail.com>
2025-05-20 08:29:54 -0300

    Questão 4

--- Lista 4/src/model/Pagamento.java
+++ Lista 4/src/model/Pagamento.java
@@ -1,5 +1,28 @@
 package model;
 
-public class Pagamento {
+abstract class Pagamento{
+    public abstract void processar(double valor);
+}
 
+class CartaoCredito extends Pagamento{
+    @Override
+    public void processar(double valor){
+        double taxa = 0.05; // 5% de taxa
+        double total = valor + (valor * taxa);
+        System.out.println("Pagamento com cartão de credito: R$ " + total);
+    }
+}
+class Boleto extends Pagamento{
+    @Override
+    public void processar(double valor){
+        double desconto = 0.1; // 10% de desconto
+        double total = valor -(valor * desconto);
+        System.out.println("Pagamento com boleto: R$ " + total);
+    }
+}
+class Pix extends Pagamento{
+    @Override
+    public void processar(double valor){
+        System.out.println("Pagamento com pix: R$ " + valor + "(sem taxas)");
+    }
 }
--- Lista 4/src/view/Main.java
+++ Lista 4/src/view/Main.java
@@ -1,4 +1,20 @@
 package view;
 
+import model.Pagamento;
+import model.CartaoCredito;
+import model.Boleto;
+import model.Pix;
+
 public class Main {
+    public static void main(String[] args) {
+        Pagamento pagamento1 = new CartaoCredito();
+        Pagamento pagamento2 = new Boleto();
+        Pagamento pagamento3 = new Pix();
+
+        double valorCompra = 100.0;
+
+        pagamento1.processar(valorCompra);
+        pagamento2.processar(valorCompra);
+        pagamento3.processar(valorCompra);
+    }
 }
